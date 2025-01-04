778b2194bc83456c303285dd8a9894b383f582a6
Breadumi <james.allred800@gmail.com>
2025-01-04 13:46:27 -0600

    added fileserver functionality for thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10 << 20
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +34,60 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
 	// TODO: implement the upload here
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't parse multipartForm", err)
+		return
+	}
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't form file", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	mediaType := fileHeader.Header.Get("Content-Type")
+	mediaType = strings.Split(mediaType, "/")[1]
+
+	dbVideo, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find video", err)
+		return
+	}
+	if userID != dbVideo.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Invalid User ID", err)
+		return
+	}
+
+	path := "./" + filepath.Join(cfg.assetsRoot, videoID.String()+"."+mediaType)
+	err = os.MkdirAll(cfg.assetsRoot, os.ModePerm)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create directory", err)
+		return
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create asset file", err)
+		return
+	}
+	_, err = io.Copy(f, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't copy asset file", err)
+		return
+	}
+
+	thumbnailURL := "http://localhost:" + cfg.port + "/assets/" + videoID.String() + "." + mediaType
+
+	dbVideo.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(dbVideo)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video metadata", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, dbVideo)
 }
